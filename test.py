b16bed7bf6c9393de3ec6400c1656543ff3e51e1
DontWorryBeHuggies <cergato@mail.ru>
2022-04-06 22:17:10 +0300

    Протестировал функцию на 3 разных зарплатах

---
+++ test.py
@@ -0,0 +1,5 @@
+from tax_function import evaluates_tax_salary_per_worker
+
+print(evaluates_tax_salary_per_worker(5000))
+print(evaluates_tax_salary_per_worker(10000))
+print(evaluates_tax_salary_per_worker(12000))
